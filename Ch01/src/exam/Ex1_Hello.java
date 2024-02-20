8f89000c02d14339e5d1d732f282385e6f705611
BabyBeast1 <137284485+BabyBeast1@users.noreply.github.com>
2024-02-20 17:49:58 +0900

    Add files via upload

---
+++ Ch01/src/exam/Ex1_Hello.java
@@ -0,0 +1,13 @@
+package exam;
+
+public class Ex1_Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+		System.out.print("Java ");
+		System.out.print("World!");
+		
+	}
+	
+}
---
+++ Ch01/src/exam/Lab_1.java
@@ -0,0 +1,12 @@
+package exam;
+
+public class Lab_1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("안녕하세요");
+		System.out.println("저는 이경호입니다");
+		
+	}
+
+}
